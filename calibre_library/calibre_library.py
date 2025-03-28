f1945ac38cd1d970d0dcfaa9daf5e380ffee4406
troberts <troberts@stanfordalumni.org>
2025-03-28 14:15:33 -0700

    Added better logging

--- calibre_library/calibre_library.py
+++ calibre_library/calibre_library.py
@@ -6,12 +6,16 @@ class CalibreLibrary:
         self._path = path
 
     def list_all_opf(self):
+        print(f'Looking for opf files in {self._path}')
+
         file_paths = []
         for dirpath, dirnames, filenames in os.walk(self._path):
             for filename in filenames:
                 if filename == 'metadata.opf':
                     file_path = os.path.join(dirpath, filename)
                     file_paths.append(file_path)
+                    print(f'Found opf: {file_path}')
+        print (f'Done looking for opf files in {self._path}')
         return file_paths
 
 
